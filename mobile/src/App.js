7736042bb7b8817a3744677187a09a90b31720b9
Tiago Bogoni <tiagojosebogoni@gmail.com>
2020-05-03 00:16:04 -0300

    Inicializado projeto

---
+++ mobile/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const App = () => {
+  return <Text>Hello World</Text>
+}
+
+export default App;
